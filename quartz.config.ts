9046eb1f4eb871b854dc09238050d3413c513f2c
ELSOM <office@elsom.co.kr>
2025-11-29 03:17:45 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "ELSOM's 스토리",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
